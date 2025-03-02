6afafe7052c6208dce9de0d92f2d8df43441e6fc
Krishna mourya <maurya1389@gmail.com>
2025-03-02 22:04:22 +0530

    update

--- base/models.py
+++ base/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class basemodel(models.model):
-    uid=models.UUIDField(primary_key=True ,editable=False , default=uuid.uuid4)
+    uid=models.UUIDField(primary_key=True ,editable=False )
     created_at =models.DateField(auto_now_add=True)
     updated_at =models.DateTimeField(auto_add=True)
 
